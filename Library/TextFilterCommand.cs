51e73cc09d86362c573662aae25636770e8b0e50
Deane Barker <deane@blendinteractive.com>
2015-01-28 14:36:08 -0600

    Enabled variable substitution.

--- Library/TextFilterCommand.cs
+++ Library/TextFilterCommand.cs
@@ -25,5 +25,24 @@ namespace BlendInteractive.TextFilterPipeline.Core
         {
             get { return CommandName.ToLower(); }
         }
+
+        public void ResolveArguments()
+        {
+            // This is a whole lot of crap to get around the problem of modifying a collection while it's being iterated...
+
+            var variablesToResolve = new Dictionary<object, string>();
+            foreach (var commandArg in CommandArgs)
+            {
+                if (CommandParser.IsVariableName(commandArg.Value))
+                {
+                    variablesToResolve.Add(commandArg.Key, commandArg.Value);
+                }
+            }
+
+            foreach (var variable in variablesToResolve)
+            {
+                CommandArgs[variable.Key] = Pipeline.GetVariable(CommandParser.StripVariablePrefix(variable.Value)).ToString();
+            }
+        }
     }
 }
