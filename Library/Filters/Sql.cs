4416631274434779231cba6cfd415312dfa2e6d5
Deane Barker <deane@blendinteractive.com>
2015-02-11 16:12:41 -0600

    Switched to XmlReader

---
+++ Library/Filters/Sql.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlendInteractive.Denina.Core.Documentation;
+using System.Configuration;
+using BlendInteractive.Denina.Core.Filters;
+
+namespace BlendInteractive.Denina.Core.Filters
+{
+    [Filters("SQL", "For working with SQL databases.")]
+    public class Sql
+    {
+        public static readonly string ALLOWED_CONNECTION_STRINGS_VARIABLE_NAME = "__allowsqlconnections";
+
+        [Filter("GetXml")]
+        public static string GetXml(string input, PipelineCommand command)
+        {
+            var allowedConnectionStrings = Pipeline.GetGlobalVariable(ALLOWED_CONNECTION_STRINGS_VARIABLE_NAME).ToString();
+            if (!allowedConnectionStrings.Split(',').Contains(command.CommandArgs.First().Value))
+            {
+                throw new Exception(String.Format("Connection string key \"{0}\" has not been authorized.", command.CommandArgs.First().Value));
+            }
+
+            var sqlCommand = new SqlCommand()
+            {
+                Connection = new SqlConnection(ConfigurationManager.ConnectionStrings[command.CommandArgs.First().Value].ToString())
+            };
+            sqlCommand.Connection.Open();
+            sqlCommand.CommandText = String.Concat(command.CommandArgs[1], " FOR XML PATH, ROOT('rows'), ELEMENTS XSINIL");
+
+            var reader = sqlCommand.ExecuteXmlReader();
+            reader.Read();
+            return reader.ReadOuterXml();
+        }
+    }
+}
+
+namespace BlendInteractive.Denina.Core
+{
+    public partial class Pipeline
+    {
+        public static void SetAllowedConnectionStrings(params string[] connectionStrings)
+        {
+            SetGlobalVariable(Sql.ALLOWED_CONNECTION_STRINGS_VARIABLE_NAME, String.Join(",", connectionStrings), true);
+        }
+    }
+}
--- Library/Filters/Xml.cs
+++ Library/Filters/Xml.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Xsl;
 using BlendInteractive.Denina.Core.Documentation;
+using BlendInteractive.Denina.Core.Filters;
 
 namespace BlendInteractive.Denina.Core.Filters
 {
     [Filters("XML", "Working with XML strings.")]
     public class Xml
     {
+        public static string XSLT_ARGUMENT_VARIABLE_NAME = "__xsltarguments";
+
         [Filter("Extract", "Extracts a single value from an XML document parsed from the input string.")]
         [ArgumentMeta(1, "XPath", true, "The XPath identifying the desired XML node. The InnerText of the resulting node will be returned.")]
         public static string ExtractFromXml(string input, PipelineCommand command)
@@ -27,8 +32,10 @@ namespace BlendInteractive.Denina.Core.Filters
         [ArgumentMeta(2, "XML", false, "The XML to transform.  If not provided, the XML is formed from the active text.")]
         public static string TransformXml(string input, PipelineCommand command)
         {
-            string xml = String.Empty;
-            string xsl = command.CommandArgs.First().Value;
+            var arguments = Pipeline.IsSetGlobally(XSLT_ARGUMENT_VARIABLE_NAME) ? (XsltArgumentList) Pipeline.GetGlobalVariable(XSLT_ARGUMENT_VARIABLE_NAME) : new XsltArgumentList();
+
+            var xml = String.Empty;
+            var xsl = command.CommandArgs.First().Value;
 
             // If there are two arguments, assume the second is XML
             if (command.CommandArgs.Count == 2)
@@ -40,19 +47,64 @@ namespace BlendInteractive.Denina.Core.Filters
                 // Otherwise, the XML is the input
                 xml = input;
             }
-
+            
+            // Set some basic parameters for our XML reading...
+            var settings = new XmlReaderSettings()
+            {
+                IgnoreComments = true,
+                IgnoreProcessingInstructions = true,
+                IgnoreWhitespace = true
+            };
+            
             // Form the XML doc from the input
-            var xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(xml);
+            var xmlReader = XmlReader.Create(new StringReader(xml), settings);
 
             // Form the XSL from the first argument
             var transform = new XslCompiledTransform();
-            transform.Load(XmlReader.Create(new StringReader(xsl)));
+            transform.Load(XmlReader.Create(new StringReader(xsl), settings));
 
             // Do the transform (we're passing in an empty XsltArgumentList as a placeholder, in case we want to do something with it later...)
             var writer = new StringWriter();
-            transform.Transform(xmlDoc, new XsltArgumentList(), writer);
-            return writer.ToString();
+            transform.Transform(xmlReader, arguments, writer);
+            return writer.ToString().Replace("\u00A0", " ");    // This is a bit of a hack. We're replacing NO BREAK SPACE with a regular space. There has to be a way to fix this in the XSLT output.
+        }
+
+        [Filter("FormatNodes")]
+        public static string FormatNodes(string input, PipelineCommand command)
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(input);
+
+            var xpath = command.CommandArgs.First().Value;
+            var template = command.CommandArgs[1];
+
+            var patterns = Regex.Matches(template, "{([^}]*)}").Cast<Match>().Select(m => m.Value).ToList();
+
+            var output = new StringBuilder();
+            foreach (XmlNode node in xmlDoc.SelectNodes(xpath))
+            {
+                var thisTemplate = String.Copy(template);
+                foreach (var pattern in patterns)
+                {
+                    var value = node.SelectSingleNode(pattern.Trim(new char[] {'}', '{'})).InnerXml;
+                    thisTemplate = thisTemplate.Replace(pattern, value);
+                }
+                output.Append(thisTemplate);
+            }
+
+            return output.ToString();
+        }
+    }
+
+}
+
+namespace BlendInteractive.Denina.Core
+{
+    public partial class Pipeline
+    {
+        public static void AddXsltArgumentList(object argumentList)
+        {
+            SetGlobalVariable(Xml.XSLT_ARGUMENT_VARIABLE_NAME, argumentList);
         }
     }
 }
