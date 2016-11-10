c3f01d5acf51e50e319fd73ba182a299d24ed0b2
eric <eric@2016DEV>
2016-11-10 21:12:12 +0100

    New syntax for json

--- Library/Filters/Json.cs
+++ Library/Filters/Json.cs
@@ -10,31 +10,15 @@ namespace DeninaSharp.Core.Filters
     public static class Json
     {
         [Filter("Extract", "Retieves the valud at a defined \"path\" into the JSON object.")]
-        [ArgumentMeta("path", true, "A dot-delimited representation of the path into the object. Each property is defined by a segment. Numeric values prepended by a tilde (\"~1\") are interpreted as the index of an array. Example: \"person.~2.name.first\".")]
-        [CodeSample("(A JSON String)", "Json.Extract -path:person.~1.name", "(The value of the \"name\" object of the second \"person\" object.)")]       
+        [ArgumentMeta("path", true, "A dot-delimited representation of the path into the object. Each property is defined by a segment. Example: \"person[2].name.first\".")]
+        [CodeSample("(A JSON String)", "Json.Extract -path:person[1].name", "(The value of the \"name\" object of the second \"person\" object.)")]       
         public static string ExtractFromJson(string input, PipelineCommand command)
         {
-            var path = command.GetArgument("path");
+            var path = command.GetArgument(key: "path");
 
-            var jobject = (JToken) JObject.Parse(input);
+            var jobject = (JToken)JObject.Parse(json: input);
 
-            foreach (string segment in path.Split('.'))
-            {
-                if (Regex.IsMatch(segment,@"\[[0-9]+\]"))
-                {
-                    jobject = jobject[Convert.ToInt32(segment.Replace("[", String.Empty).Replace("]", String.Empty))];
-                    continue;
-                }
-
-                if (jobject[segment] is JValue)
-                {
-                    return jobject[segment].ToString();
-                }
-
-                jobject = jobject[segment];
-            }
-
-            return String.Empty;
+            return (string)jobject.SelectToken(path: path);
         }
     }
 }
