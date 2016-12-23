7b5627df991f744a84ac6e6db56491cb14a759a2
Deane Barker <deane@blendinteractive.com>
2016-12-23 09:53:28 -0600

    Added tests for event handling.

---
+++ Tests/EventTests.cs
@@ -0,0 +1,30 @@
+﻿using DeninaSharp.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Pipeline.Init();
+        }
+
+        [TestMethod]
+        public void PipelineComplete()
+        {
+            Pipeline.PipelineComplete += (s, e) => { e.Value = "foo"; }; // This should just totally replace the value to prove the event fired
+            var pipeline = new Pipeline();
+            var result = pipeline.Execute("bar");  // Without the event handler, this should just pass "bar" through
+
+            Assert.AreEqual("foo", result);
+        }
+    }
+}
