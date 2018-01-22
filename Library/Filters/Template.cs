1ccc4b005d7021f3a5cfa1cc3ce8dc9ce58decd5
Deane Barker <deane@blendinteractive.com>
2018-01-22 14:56:50 -0500

    Added "Template" category with DotLiquid templating

---
+++ Library/Filters/Template.cs
@@ -0,0 +1,153 @@
+﻿using DeninaSharp.Core;
+using DeninaSharp.Core.Documentation;
+using DotLiquid;
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace BlendInteractive.Denina.Core.Filters
+{
+    [Filters("Template", "Templating output with DotLiquid.")]
+    public static class Template
+    {
+        [Filter("FromText", "Processes a DotLiquid template with the input text as the 'data' variable.")]
+        [Requires("DotLiquid.Template, DotLiquid", "DotLiquid is an open-source templating library.")]
+        [ArgumentMeta("template", true, "A DotLiquid template string.")]
+        public static string FromText(string input, PipelineCommand command)
+        {
+            var template = command.GetArgument("template");
+
+            var parsedTemplate = DotLiquid.Template.Parse(template);
+            return parsedTemplate.Render(Hash.FromAnonymousObject(
+                    new { data = input }
+                ));
+        }
+
+        [Filter("FromXml", "Processes a DotLiquid template with the 'data' variable representing either the entire input XML, or a collection of XML nodes.")]
+        [Requires("DotLiquid.Template, DotLiquid", "DotLiquid is an open-source templating library.")]
+        [ArgumentMeta("template", true, "A DotLiquid template string.")]
+        [ArgumentMeta("xpath", false, "If supplied, the XPath will be executed on the XML to return a node list. The 'data' variable in the DotLiquid template will be a collection of these nodes. If not supplied, the 'data variable will be the entire XML as a single object.")]
+        public static string FromXml(string input, PipelineCommand command)
+        {
+            var xpath = command.GetArgument("xpath", string.Empty);
+            var template = command.GetArgument("template");
+
+            var parsedTemplate = DotLiquid.Template.Parse(template);
+
+            // Parse the incoming XML
+            var xml = new XmlDocument();
+            try
+            {
+                xml.LoadXml(input);
+            }
+            catch (Exception e)
+            {
+                throw new DeninaException("XML Not Valid", e);
+            }
+
+            if (!String.IsNullOrWhiteSpace(xpath))
+            {
+                // If they supplied XPath, then we need to run it against the input to find multiple, individual XML nodes
+                // We feed those to the template as a collection
+                // "data" in the template is a List<XmlNode>
+
+                // Load the XML nodes into a list of objects
+                var templateData = new List<Drops.XmlNode>();
+                foreach (XmlElement node in xml.SelectNodes(xpath))
+                {
+                    templateData.Add(new Drops.XmlNode(node.OuterXml));
+                }
+
+                return parsedTemplate.Render(Hash.FromAnonymousObject(
+                    new { data = templateData }
+                ));
+            }
+            else
+            {
+                // No xpath, so the entire input is feed to the template as a single object
+                // "data" in the template is a single XmlNode
+                return parsedTemplate.Render(Hash.FromAnonymousObject(
+                    new { data = new Drops.XmlNode(xml) }
+                ));
+            }
+        }
+
+        public static class Drops
+        {
+            // Represents an XML document
+            // Allows addressing content in a DotLiquid template
+            // ex: "data.person.name.first" to find the text in the XML node at "/person/name/first"
+            public class XmlNode : Drop
+            {
+                private XmlElement doc;
+
+                public XmlNode(XmlDocument xml)
+                {
+                    doc = xml.DocumentElement;
+                }
+
+                public XmlNode(XmlElement xml)
+                {
+                    doc = xml;
+                }
+
+                public XmlNode(string xml)
+                {
+                    var xmlDoc = new XmlDocument();
+                    try
+                    {
+                        xmlDoc.LoadXml(xml);
+                    }
+                    catch (XmlException e) when (e.Message.Contains("multiple root elements"))
+                    {
+                        xmlDoc.LoadXml($"<root>{xml}</root>");
+                    }
+                    doc = xmlDoc.DocumentElement;
+                }
+
+                private string GetPathToNode(string path)
+                {
+                    // Yes, this just returns the input
+                    // It might change in the future, hence the abstraction
+                    return $"{path}";
+                }
+
+                private string GetPathToTextNode(string path)
+                {
+                    return $"{path}/text()";
+                }
+
+                public override object BeforeMethod(string method)
+                {
+                    if (method.StartsWith("_"))
+                    {
+                        // Attribute selector
+                        // If it starts with an underscore, swap for an "@"
+                        // (You can't start DotLiquid methods with a "@", so we have to hack with the underscore.)
+                        method = string.Concat("@", method.Substring(1));
+                    }
+
+                    if (doc.SelectSingleNode(GetPathToNode(method)) == null)
+                    {
+                        // We have nothing for this node, return an empty string
+                        return string.Empty;
+                    }
+
+                    if (doc.SelectSingleNode(GetPathToTextNode(method)) != null)
+                    {
+                        // We have inner text for this node, return it
+                        return doc.SelectSingleNode(GetPathToTextNode(method)).InnerText;
+                    }
+
+                    // We have a new node for this method, so return a new node so they can drill down further
+                    return new XmlNode((XmlElement)doc.SelectSingleNode(GetPathToNode(method)));
+                }
+            }
+        }
+    }
+
+
+
+
+    // Define other methods and classes here
+}
---
+++ Tests/TemplateTests.cs
@@ -0,0 +1,58 @@
+﻿using DeninaSharp.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class TemplateTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Pipeline.Init();
+        }
+
+        [TestMethod]
+        public void FromText()
+        {
+            var pipeline = new Pipeline();
+            pipeline.AddCommand("Template.FromText -template:\"Foo {{ data }}\"");
+            string result = pipeline.Execute("Bar");
+
+            Assert.AreEqual("Foo Bar", result);
+        }
+
+        [TestMethod]
+        public void FromXml()
+        {
+            //var pipeline = new Pipeline();
+            //pipeline.AddCommand("Template.FromXml -template:\"Foo {{ data.name }}\"");
+            //string result = pipeline.Execute("<person><name>Bar</name></person>");
+
+            //Assert.AreEqual("Foo Bar", result);
+
+            var pipeline2 = new Pipeline();
+            pipeline2.AddCommand("Template.FromXml -template:\"Foo {{ data.name.first }} {{ data.name.last }}\"");
+            string result2 = pipeline2.Execute("<person><name><first>Bar</first><last>Baz</last></name></person>");
+
+            Assert.AreEqual("Foo Bar Baz", result2);
+        }
+
+        [TestMethod]
+        public void FromXmlNodes()
+        {
+            var template = "Foo {% for thing in data %}{{ thing.name }} {% endfor %}";
+            var pipeline = new Pipeline();
+            pipeline.SetVariable("__template", template);
+            pipeline.AddCommand("Template.FromXml -xpath://person -template:$__template");
+            string result = pipeline.Execute("<root><person><name>Bar</name></person><person><name>Baz</name></person></root>");
+
+            Assert.AreEqual("Foo Bar Baz ", result);
+        }
+    }
+}
