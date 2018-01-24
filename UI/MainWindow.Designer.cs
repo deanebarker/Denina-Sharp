622df6a3a4cd485befcd3fb59033d9a1d270201d
Deane Barker <deane@blendinteractive.com>
2018-01-24 05:41:44 -0600

    Added last used settings, selected command execution, better window sizing

--- UI/MainWindow.Designer.cs
+++ UI/MainWindow.Designer.cs
@@ -30,25 +30,18 @@ namespace UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.PipelineCommands = new System.Windows.Forms.TextBox();
             this.ExecuteButton = new System.Windows.Forms.Button();
-            this.PipelineResults = new System.Windows.Forms.TextBox();
             this.Status = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.PipelineResults = new System.Windows.Forms.TextBox();
+            this.PipelineCommands = new System.Windows.Forms.TextBox();
             this.InputTextbox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // PipelineCommands
-            // 
-            this.PipelineCommands.Font = new System.Drawing.Font("Courier New", 10F);
-            this.PipelineCommands.Location = new System.Drawing.Point(386, 11);
-            this.PipelineCommands.Multiline = true;
-            this.PipelineCommands.Name = "PipelineCommands";
-            this.PipelineCommands.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.PipelineCommands.Size = new System.Drawing.Size(386, 540);
-            this.PipelineCommands.TabIndex = 0;
             // 
             // ExecuteButton
             // 
+            this.ExecuteButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.ExecuteButton.Location = new System.Drawing.Point(512, 591);
             this.ExecuteButton.Name = "ExecuteButton";
             this.ExecuteButton.Size = new System.Drawing.Size(106, 48);
@@ -56,18 +49,6 @@ namespace UI
             this.ExecuteButton.Text = "Execute";
             this.ExecuteButton.UseVisualStyleBackColor = true;
             this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
-            // 
-            // PipelineResults
-            // 
-            this.PipelineResults.BackColor = System.Drawing.Color.White;
-            this.PipelineResults.Font = new System.Drawing.Font("Courier New", 10F);
-            this.PipelineResults.Location = new System.Drawing.Point(791, 11);
-            this.PipelineResults.Multiline = true;
-            this.PipelineResults.Name = "PipelineResults";
-            this.PipelineResults.ReadOnly = true;
-            this.PipelineResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.PipelineResults.Size = new System.Drawing.Size(350, 667);
-            this.PipelineResults.TabIndex = 2;
             // 
             // Status
             // 
@@ -78,39 +59,89 @@ namespace UI
             this.Status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.Status.UseMnemonic = false;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.PipelineResults, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.PipelineCommands, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.InputTextbox, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(22, 22);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1106, 545);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // PipelineResults
+            // 
+            this.PipelineResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PipelineResults.BackColor = System.Drawing.Color.White;
+            this.PipelineResults.Font = new System.Drawing.Font("Courier New", 10F);
+            this.PipelineResults.Location = new System.Drawing.Point(739, 3);
+            this.PipelineResults.Multiline = true;
+            this.PipelineResults.Name = "PipelineResults";
+            this.PipelineResults.ReadOnly = true;
+            this.PipelineResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.PipelineResults.Size = new System.Drawing.Size(364, 539);
+            this.PipelineResults.TabIndex = 7;
+            // 
+            // PipelineCommands
+            // 
+            this.PipelineCommands.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PipelineCommands.Font = new System.Drawing.Font("Courier New", 10F);
+            this.PipelineCommands.Location = new System.Drawing.Point(371, 3);
+            this.PipelineCommands.Multiline = true;
+            this.PipelineCommands.Name = "PipelineCommands";
+            this.PipelineCommands.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.PipelineCommands.Size = new System.Drawing.Size(362, 539);
+            this.PipelineCommands.TabIndex = 6;
+            this.PipelineCommands.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PipelineCommands_KeyDown);
+            // 
             // InputTextbox
             // 
+            this.InputTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.InputTextbox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.InputTextbox.Location = new System.Drawing.Point(12, 12);
+            this.InputTextbox.Location = new System.Drawing.Point(3, 3);
             this.InputTextbox.Multiline = true;
             this.InputTextbox.Name = "InputTextbox";
             this.InputTextbox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.InputTextbox.Size = new System.Drawing.Size(350, 666);
-            this.InputTextbox.TabIndex = 4;
+            this.InputTextbox.Size = new System.Drawing.Size(362, 539);
+            this.InputTextbox.TabIndex = 5;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1153, 692);
-            this.Controls.Add(this.InputTextbox);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.Status);
-            this.Controls.Add(this.PipelineResults);
             this.Controls.Add(this.ExecuteButton);
-            this.Controls.Add(this.PipelineCommands);
             this.Name = "MainWindow";
             this.Text = "Text Filter Pipeline Tester";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox PipelineCommands;
         private System.Windows.Forms.Button ExecuteButton;
-        private System.Windows.Forms.TextBox PipelineResults;
         private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox PipelineResults;
+        private System.Windows.Forms.TextBox PipelineCommands;
         private System.Windows.Forms.TextBox InputTextbox;
     }
 }
--- UI/MainWindow.cs
+++ UI/MainWindow.cs
@@ -3,26 +3,37 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
 using DeninaSharp.Core;
+using Microsoft.Win32;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
 
 namespace UI
 {
     public partial class MainWindow : Form
     {
+        private const string settingsFileLocation = "settings.xml";
+        private const string lastInputSettingName = "lastInput";
+        private const string lastCommandsSettingName = "lastCommands";
+
         public MainWindow()
         {
             InitializeComponent();
+            ReadSettings();
         }
 
         private void ExecuteButton_Click(object sender, EventArgs e)
         {
             PipelineResults.BackColor = Color.FromName("White");
 
+            var commands = String.IsNullOrWhiteSpace(PipelineCommands.SelectedText) ? PipelineCommands.Text : PipelineCommands.SelectedText;
+
             try
             {
                 var stopWatch = new Stopwatch();
                 stopWatch.Start();
                 var pipeline = new Pipeline();
-                pipeline.AddCommand(PipelineCommands.Text);
+                pipeline.AddCommand(commands);
                 long parseTime = stopWatch.ElapsedMilliseconds;
                 PipelineResults.Text = pipeline.Execute(InputTextbox.Text);
                 long executionTime = stopWatch.ElapsedMilliseconds;
@@ -35,6 +46,53 @@ namespace UI
                 PipelineResults.Text = String.Join(Environment.NewLine, exception.Message, exception.StackTrace);
                 PipelineResults.BackColor = Color.FromName("LightPink");
             }
+
+            WriteSettings();
         }
+
+        private void PipelineCommands_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.E && Control.ModifierKeys == Keys.Control)
+            {
+                ExecuteButton_Click(sender, new EventArgs());
+            }
+        }
+
+        private void WriteSettings()
+        {
+            using (var xml = XmlWriter.Create(settingsFileLocation))
+            {
+                xml.WriteStartDocument();
+                xml.WriteStartElement("settings");
+                xml.WriteStartElement(lastInputSettingName);
+                xml.WriteCData(InputTextbox.Text);
+                xml.WriteEndElement();
+                xml.WriteStartElement(lastCommandsSettingName);
+                xml.WriteCData(PipelineCommands.Text);
+                xml.WriteEndElement();
+                xml.WriteEndDocument();
+                xml.Flush();
+                xml.Close();
+            }
+        }
+
+        private void ReadSettings()
+        {
+            if (File.Exists(settingsFileLocation))
+            {
+                try
+                {
+                    var xml = XDocument.Load(settingsFileLocation);
+                    InputTextbox.Text = xml.Root.Element(lastInputSettingName).Value;
+                    PipelineCommands.Text = xml.Root.Element(lastCommandsSettingName).Value;
+                }
+                catch (Exception e)
+                {
+                    // Just don't read the settings, I guess
+                }
+            }
+        }
+
+        
     }
 }
