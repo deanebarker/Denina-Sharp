6c235706e628d9b03a42872f54b1ee1a2185ecda
Deane Barker <deane@blendinteractive.com>
2018-07-31 09:08:22 -0500

    Added version info to the assembly

---
+++ Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DeninaSharp")]
+[assembly: AssemblyDescription("A text processing pipeline")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Blend Interactive")]
+[assembly: AssemblyProduct("BlendInteractive.Denina.Core.Properties")]
+[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c614efa-66db-43dc-889e-dc345ef97c83")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
